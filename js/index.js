a7c57beabab5ed608b8a1c3bb20d2bee31f7523a
Magda <magdalenacueto04@hotmail.com>
2022-02-12 13:05:57 -0300

    Agregué archivo index.js

---
+++ js/index.js
@@ -0,0 +1,19 @@
+//Comentarios JavaScript
+
+//Variables en JavaScript, se guardan en la memoria RAM
+
+//Declaro una variable, le asigno un espacio
+let nombre;
+
+var apellido;
+
+//Declaración y asignación de valor a una variable
+const DNI = 123456789;
+
+//Asignar un valor a la variable (se pueden usar ambos tipos de comillas)
+nombre = 'Magdalena';
+apellido = "Cueto";
+
+let pais = "Argentina";
+
+console.log("Mi nombre es:" + nombre + apellido + "y vivo en la" + pais +"." + " Mi DNI es: " + DNI + ".");
